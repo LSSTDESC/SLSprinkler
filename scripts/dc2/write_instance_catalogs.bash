f461fe6c214b94316c13ec136a29e360d4a437bb
John B Kalmbach <jbkalmbach@gmail.com>
2020-04-01 13:26:10 -0700

    Started DDF instance catalog writing script.

---
+++ scripts/dc2/write_instance_catalogs.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# ObsHistId
+visit_id=$1
+
+# Point to sims_GCRCatSimInterface using env variable set by lsst stack
+gcr_catsim_dir=$SIMS_GCRCATSIMINTERFACE_DIR
+
+# Output for instance catalog files
+out_dir=$2
+
+# Location of lensed agn truth catalog
+agn_truth_cat=$3
+
+# Location of lensed sne truth catalog
+sne_truth_cat=$4
+
+# Location to store SED files for lensed SNe
+sne_sed_dir=$5
+
+python $gcr_catsim_dir/bin.src/generateInstCat.py \
+    --config_file $gcr_catsim_dir/workspace/run2.1/test_agn_config_file_2.1.ddf.json \
+    --fov 0.05 --ids $visit_id --agn_threads 2 --out_dir $out_dir
+
+python create_agn_ic.py \
+    --obs_db /global/projecta/projectdirs/lsst/groups/SSim/DC2/DDF_cadences/ddf_cadence.db \
+    --obs_id $visit_id --agn_truth_cat $agn_truth_cat --file_out $out_dir/lensed_agn_$visit_id.txt
+
+python create_sne_ic.py \
+    --obs_db /global/projecta/projectdirs/lsst/groups/SSim/DC2/DDF_cadences/ddf_cadence.db \
+    --obs_id $visit_id --sne_truth_cat $sne_truth_cat --file_out $out_dir/lensed_sne_$visit_id.txt \
+    --sed_out $out_dir/glsne_sed
