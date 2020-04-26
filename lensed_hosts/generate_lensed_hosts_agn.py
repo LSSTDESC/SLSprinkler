9ffdff715dcb3d4a8f4f81a3608774f06ad02747
Jim Chiang <jchiang@slac.stanford.edu>
2020-04-26 15:08:14 -0700

    refactor create_cats_[sne,agn] and load_in_data_[sne,agn] into a single LensedHostGenerator class

--- lensed_hosts/generate_lensed_hosts_agn.py
+++ lensed_hosts/generate_lensed_hosts_agn.py
@@ -1,217 +1,42 @@
 #!/usr/bin/env python
-import numpy as np
+"""Script to generate AGN lensed hosts."""
 import os
-import argparse
-import pylab as pl
-import subprocess as sp
-import astropy.io.fits as pyfits
-import pandas as pd
-import scipy.special as ss
-import om10_lensing_equations as ole
-import sqlite3 as sql
 import sys
-from lensed_hosts_utils import generate_lensed_host
+import argparse
+import numpy as np
+from lensed_hosts_utils import LensedHostGenerator
 
 # Have numpy raise exceptions for operations that would produce nan or inf.
 np.seterr(invalid='raise', divide='raise', over='raise')
 
-datadefault = 'truth_tables'
-outdefault = 'outputs'
-
-parser = argparse.ArgumentParser(description='The location of the data directory')
-parser.add_argument("--datadir", dest='datadir', type=str, default = datadefault,
-                    help='Location of data directory (containing truth tables)')
-parser.add_argument("--outdir", dest='outdir', type=str, default = outdefault,
+parser = argparse.ArgumentParser(description="Script to make AGN lensed hosts")
+parser.add_argument("--datadir", type=str, default='truth_tables',
+                    help='Location of directory containing truth tables')
+parser.add_argument("--outdir", type=str, default='outputs',
                     help='Output location for FITS stamps')
 parser.add_argument("--pixel_size", type=float, default=0.01,
                     help='Pixel size in arcseconds')
 parser.add_argument("--num_pix", type=int, default=1000,
                     help='Number of pixels in x- or y-direction')
-
 args = parser.parse_args()
-datadir = args.datadir
-outdir = args.outdir
 
+host_truth_file = os.path.join(args.datadir, 'host_truth.db')
+lens_truth_file = os.path.join(args.datadir, 'lens_truth.db')
+generator = LensedHostGenerator(host_truth_file, lens_truth_file, 'agn',
+                                args.outdir, pixel_size=args.pixel_size,
+                                num_pix=args.num_pix)
 
-def load_in_data_agn():
-
-    """
-    Reads in catalogs of host galaxy bulge and disk as well as om10 lenses
-
-    Returns:
-    -----------
-    lens_list: data array for lenses.  Includes t0, x, y, sigma, gamma, e, theta_e
-    ahb_purged: Data array for galaxy bulges.  Includes prefix, uniqueId, raPhoSim, decPhoSim, phosimMagNorm
-    ahd_purged: Data array for galaxy disks.  Includes prefix, uniqueId, raPhoSim, decPhoSim, phosimMagNorm
-
-    """
-    conn = sql.connect(os.path.join(datadir,'host_truth.db'))
-    agn_host = pd.read_sql_query("select * from agn_hosts;", conn)
-
-    conn2 = sql.connect(os.path.join(datadir,'lens_truth.db'))
-    agn_lens = pd.read_sql_query("select * from agn_lens;", conn2)
-
-    idx = agn_host['image_number'] == 0
-    ahb_purged = agn_host[idx]
-
-    lens_list = agn_lens
-
-    return lens_list, ahb_purged
-
-
-def create_cats_agns(index, hdu_list, ahb_list):
-    """
-    Takes input catalogs and isolates lensing parameters as well as ra and dec of lens
-
-    Parameters:
-    -----------
-    index: int
-        Index for pandas data frame
-    hdu_list:
-        row of data frame that contains lens parameters
-    ahb_list:
-        row of data frame that contains lens galaxy parameters for the galactic bulge
-
-    Returns:
-    -----------
-    lens_cat: Data array that includes lens parameters
-    srcsP_bulge: Data array that includes parameters for galactic bulge
-    srcsP_disk: Data array that includes parameters for galactic disk
-    """
-
-    df_inner = pd.merge(ahb_list, hdu_list, on='lens_cat_sys_id', how='inner')
-
-#    for col in df_inner.columns:
-#        print(col)
-    dc2_sys_id_tokens = df_inner['dc2_sys_id_x'][index].split('_')
-    UID_lens = '_'.join((dc2_sys_id_tokens[0], 'host', dc2_sys_id_tokens[1],
-                         str(df_inner['image_number'][index])))
-    twinkles_ID = UID_lens
-    cat_id = df_inner['unique_id_x'][index]
-    Ra_lens = df_inner['ra_lens_x'][index]
-    Dec_lens = df_inner['dec_lens_x'][index]
-    ys1 = df_inner['x_src'][index]
-    ys2 = df_inner['y_src'][index]
-    ximg = df_inner['x_img'][index]
-    yimg = df_inner['y_img'][index]
-    xl1 = 0.0
-    xl2 = 0.0
-    ra_lens_check = df_inner['ra_lens_y'][index]
-    dec_lens_check = df_inner['dec_lens_y'][index]
-    lid = df_inner['lens_cat_sys_id'][index]
-    vd = df_inner['vel_disp_lenscat'][index]
-    zd = df_inner['redshift_y'][index]
-    ql = 1.0 - df_inner['ellip_lens'][index]
-    phi = df_inner['position_angle_y'][index]
-    ext_shr = df_inner['gamma_lenscat'][index]
-    ext_phi = df_inner['phig_lenscat'][index]
-
-    #----------------------------------------------------------------------------
-    lens_cat = {'xl1'        : xl1,
-                'xl2'        : xl2,
-                'ql'         : ql,
-                'vd'         : vd,
-                'phl'        : phi,
-                'gamma'      : ext_shr,
-                'phg'        : ext_phi,
-                'zl'         : zd,
-                'ximg'       : ximg,
-                'yimg'       : yimg,
-                'twinklesid' : twinkles_ID,
-                'lensid'     : lid,
-                'index'      : index,
-                'UID_lens'   : UID_lens,
-                'Ra_lens'    : Ra_lens,
-                'Dec_lens'   : Dec_lens,
-                'cat_id'     : cat_id}
-
-    #----------------------------------------------------------------------------
-
-    mag_src_b_u = df_inner['magnorm_bulge_u'][index]
-    mag_src_b_g = df_inner['magnorm_bulge_g'][index]
-    mag_src_b_r = df_inner['magnorm_bulge_r'][index]
-    mag_src_b_i = df_inner['magnorm_bulge_i'][index]
-    mag_src_b_z = df_inner['magnorm_bulge_z'][index]
-    mag_src_b_y = df_inner['magnorm_bulge_y'][index]
-
-    qs_b = df_inner['minor_axis_bulge'][index]/df_inner['major_axis_bulge'][index]
-    Reff_src_b = np.sqrt(df_inner['minor_axis_bulge'][index]*df_inner['major_axis_bulge'][index])
-    phs_b = df_inner['position_angle_x'][index]
-    ns_b = df_inner['sindex_bulge'][index]
-    zs_b = df_inner['redshift_x'][index]
-    sed_src_b = df_inner['sed_bulge_host'][index]
-
-    srcsP_bulge = {'ys1'          : ys1,
-                   'ys2'          : ys2,
-                   'mag_src_u'      : mag_src_b_u,
-                   'mag_src_g'      : mag_src_b_g,
-                   'mag_src_r'      : mag_src_b_r,
-                   'mag_src_i'      : mag_src_b_i,
-                   'mag_src_z'      : mag_src_b_z,
-                   'mag_src_y'      : mag_src_b_y,
-                   'Reff_src'     : Reff_src_b,
-                   'qs'           : qs_b,
-                   'phs'          : phs_b,
-                   'ns'           : ns_b,
-                   'zs'           : zs_b,
-                   'sed_src'      : sed_src_b,
-                   'components'   : 'bulge'}
-
-    #----------------------------------------------------------------------------
-    mag_src_d_u = df_inner['magnorm_disk_u'][index]
-    mag_src_d_g = df_inner['magnorm_disk_g'][index]
-    mag_src_d_r = df_inner['magnorm_disk_r'][index]
-    mag_src_d_i = df_inner['magnorm_disk_i'][index]
-    mag_src_d_z = df_inner['magnorm_disk_z'][index]
-    mag_src_d_y = df_inner['magnorm_disk_y'][index]
-
-    qs_d = df_inner['minor_axis_disk'][index]/df_inner['major_axis_disk'][index]
-    Reff_src_d = np.sqrt(df_inner['minor_axis_disk'][index]*df_inner['major_axis_disk'][index])
-    phs_d = df_inner['position_angle_x'][index]
-    ns_d = df_inner['sindex_disk'][index]
-    zs_d = df_inner['redshift_x'][index]
-    sed_src_d = df_inner['sed_disk_host'][index]
-
-    srcsP_disk = {'ys1'          : ys1,
-                  'ys2'          : ys2,
-                  'mag_src_u'      : mag_src_d_u,
-                  'mag_src_g'      : mag_src_d_g,
-                  'mag_src_r'      : mag_src_d_r,
-                  'mag_src_i'      : mag_src_d_i,
-                  'mag_src_z'      : mag_src_d_z,
-                  'mag_src_y'      : mag_src_d_y,
-                  'Reff_src'     : Reff_src_d,
-                  'qs'           : qs_d,
-                  'phs'          : phs_d,
-                  'ns'           : ns_d,
-                  'zs'           : zs_d,
-                  'sed_src'      : sed_src_d,
-                  'components'   : 'disk'}
-
-    return lens_cat, srcsP_bulge, srcsP_disk
-
-
-if __name__ == '__main__':
-
-    dsx = args.pixel_size  # pixel size per side, arcseconds
-    nnn = args.num_pix  # number of pixels per side
-    xi1, xi2 = ole.make_r_coor(nnn, dsx)
-
-    hdulist, ahb = load_in_data_agn()
-
-    #hdulist is the list of lens parameters
-    #ahb is the list of galaxy bulge and disk parameters
-
-    message_row = 0
-    message_freq = 50
-    for i, row in hdulist.iterrows():
-        if i >= message_row:
-            print ("working on system ", i , "of", max(hdulist.index))
-            message_row += message_freq
-        try:
-            lensP, srcPb, srcPd = create_cats_agns(i, hdulist, ahb)
-            generate_lensed_host(xi1, xi2, lensP, srcPb, srcPd, dsx,
-                                 outdir, 'agn')
-        except RuntimeError as eobj:
-            print(eobj)
-        sys.stdout.flush()
+message_row = 0
+message_freq = 50
+num_rows = len(generator)
+for i in range(num_rows):
+    if i > 10:
+        break
+    if i >= message_row:
+        print("working on system ", i, "of", num_rows)
+        message_row += message_freq
+    try:
+        generator.create(i)
+    except RuntimeError as eobj:
+        print(eobj)
+    sys.stdout.flush()
--- lensed_hosts/lensed_hosts_utils.py
+++ lensed_hosts/lensed_hosts_utils.py
@@ -1,9 +1,14 @@
 import os
+import sqlite3
 import numpy as np
+import scipy.special as ss
+import pylab as pl
+import pandas as pd
 from astropy.io import fits
 import om10_lensing_equations as ole
 
-__all__ = ['generate_lensed_host', 'lensed_sersic_2d']
+__all__ = ['LensedHostGenerator', 'generate_lensed_host',
+           'lensed_sersic_2d', 'random_location']
 
 
 def boundary_max(data):
@@ -128,3 +133,230 @@ def generate_lensed_host(xi1, xi2, lens_P, srcP_b, srcP_d, dsx, outdir,
     outfile = os.path.join(outdir, f'{object_type}_lensed_disks',
                            f"{lens_id}_disk.fits")
     write_fits_stamp(lensed_image_d, magnorms, lens_id, 'disk', dsx, outfile)
+
+
+def random_location(Reff_src, qs, phs, ns, rng=None):
+    """Sample a random (x, y) location from the surface brightness
+    profile of the galaxy. The input parameters are Sersic parameters for the host galaxy.
+    Parameters:
+    -----------
+    Reff_src: float
+        the effective radius in arcseconds, the radius within which half of the light is contained
+    qs: float
+        axis ratio of the source, b/a
+    phs: float
+        position angle of the galaxy in degrees
+    ns: int
+        Sersic index
+    rng: numpy.random.RandomState [None]
+        RandomState object to use for generating random draws from [0, 1).
+        If None, then create a RandomState with default seeding.
+
+    Returns:
+    -----------
+    dx: horizontal coordinate of random location (pixel coordinates)
+    dy: vertical coordinate of random location (pixel coordinates)
+    """
+    if rng is None:
+        rng = np.random.RandomState()
+
+    phs_rad = np.deg2rad(phs-90)
+
+    bn = ss.gammaincinv(2. * ns, 0.5)
+    z = rng.random_sample()
+    x = ss.gammaincinv(2. * ns, z)
+    R = (x / bn)**ns * Reff_src
+    theta = rng.random_sample() * 2 * np.pi
+
+    xp, yp = R * np.cos(theta), R * np.sin(theta)
+    xt = xp * np.sqrt(qs)
+    yt = yp / np.sqrt(qs)
+    dx, dy = np.linalg.solve([[np.cos(phs_rad), np.sin(phs_rad)],
+                             [-np.sin(phs_rad), np.cos(phs_rad)]],
+                             [xt, yt])
+    return dx, dy
+
+
+def check_random_locations():
+    """Defines a random location to compare to"""
+
+    npoints = 100000
+    Reff_disk = 0.2
+    qs_disk = 0.3
+    phs_disk = 8.
+    ns_disk = 1.0
+
+    x_d = np.zeros(npoints)
+    y_d = np.zeros(npoints)
+
+    for i in range(npoints):
+        x_d[i], y_d[i] = random_location(Reff_disk, qs_disk, phs_disk, ns_disk)
+
+
+    bsz = 5.0
+    nnn = 1000  # number of pixels per side
+    dsx = bsz/nnn
+    xi1, xi2 = ole.make_r_coor(nnn, dsx)
+
+    src_disk = ole.sersic_2d(xi1,xi2,0.0,0.0,Reff_disk,qs_disk,phs_disk,ns_disk)
+    src_disk_norm = src_disk/(np.sum(src_disk)*dsx*dsx)
+
+    src_disk_px = np.sum(src_disk, axis=1)
+    src_disk_norm_px = src_disk_px/(np.sum(src_disk_px)*dsx)
+
+    src_disk_py = np.sum(src_disk, axis=0)
+    src_disk_norm_py = src_disk_py/(np.sum(src_disk_py)*dsx)
+
+    xsteps = xi1[:,0]
+    #---------------------------------------------------------------
+
+    from matplotlib.ticker import NullFormatter
+
+    nullfmt = NullFormatter()         # no labels
+
+    # definitions for the axes
+    left, width = 0.1, 0.65
+    bottom, height = 0.1, 0.65
+    bottom_h = left_h = left + width + 0.02
+
+    rect_scatter = [left, bottom, width, height]
+    rect_histx = [left, bottom_h, width, 0.2]
+    rect_histy = [left_h, bottom, 0.2, height]
+
+    # start with a rectangular Figure
+    pl.figure(1, figsize=(8, 8))
+
+    axScatter = pl.axes(rect_scatter)
+    axHistx = pl.axes(rect_histx)
+    axHisty = pl.axes(rect_histy)
+
+    # no labels
+    axHistx.xaxis.set_major_formatter(nullfmt)
+    axHisty.yaxis.set_major_formatter(nullfmt)
+
+    # the scatter plot:
+    axScatter.scatter(x_d, y_d)
+    axScatter.contour(xi1, xi2, src_disk, colors=['k',])
+
+    # now determine nice limits by hand:
+    binwidth = 0.02
+    xymax = max(np.max(np.abs(x_d)), np.max(np.abs(y_d)))
+    lim = (int(xymax/binwidth) + 1) * binwidth
+
+    axScatter.set_xlim((-lim, lim))
+    axScatter.set_ylim((-lim, lim))
+
+    bins = np.arange(-lim, lim + binwidth, binwidth)
+    axHistx.hist(x_d, bins=bins, density=1)
+    axHistx.plot(xsteps, src_disk_norm_px, 'k-')
+
+    axHisty.hist(y_d, bins=bins, density=1,orientation='horizontal')
+    axHisty.plot(src_disk_norm_py, xsteps, 'k-')
+
+    axHistx.set_xlim(axScatter.get_xlim())
+    axHisty.set_ylim(axScatter.get_ylim())
+
+    return 0
+
+
+class LensedHostGenerator:
+    """Class to generate lensed hosts."""
+    def __init__(self, host_truth_file, lens_truth_file, obj_type, outdir,
+                 pixel_size=0.04, num_pix=250, rng=None):
+        with sqlite3.connect(host_truth_file) as conn:
+            host_df = pd.read_sql(f'select * from {obj_type}_hosts', conn) \
+                        .query('image_number==0')
+        with sqlite3.connect(lens_truth_file) as conn:
+            lens_df = pd.read_sql(f'select * from {obj_type}_lens', conn)
+        self.df = pd.merge(host_df, lens_df, on='lens_cat_sys_id', how='inner')
+        self.obj_type = obj_type
+        self.outdir = outdir
+        self.pixel_size = pixel_size
+        self.xi1, self.xi2 = ole.make_r_coor(num_pix, pixel_size)
+        self.rng = rng
+
+    def create(self, index):
+        """Generate the lensed host for the object pointed at by `index`"""
+        lens_params, bulge_params, disk_params = self._extract_params(index)
+        generate_lensed_host(self.xi1, self.xi2, lens_params, bulge_params,
+                             disk_params, self.pixel_size, self.outdir,
+                             self.obj_type)
+
+    def _extract_params(self, index):
+        row = self.df.iloc[index]
+
+        if not (np.isfinite(row['x_src']) and np.isfinite(row['y_src'])):
+            raise RuntimeError('x_src or y_src is not finite for '
+                               f'lens id {row["lens_cat_sys_id"]}')
+
+        dc2_sys_id_tokens = row['dc2_sys_id_x'].split('_')
+        UID_lens = '_'.join((dc2_sys_id_tokens[0], 'host', dc2_sys_id_tokens[1],
+                             str(row['image_number'])))
+        twinkles_ID = UID_lens
+        lens_cat = {'xl1': 0,
+                    'xl2': 0,
+                    'ql': 1.0 - row['ellip_lens'],
+                    'vd': row['vel_disp_lenscat'],
+                    'phl': row['position_angle_y'],
+                    'gamma': row['gamma_lenscat'],
+                    'phg': row['phig_lenscat'],
+                    'zl': row['redshift_y'],
+                    'ximg': row['x_img'],
+                    'yimg': row['y_img'],
+                    'twinklesid': twinkles_ID,
+                    'lensid': row['lens_cat_sys_id'],
+                    'index': index,
+                    'UID_lens': UID_lens,
+                    'Ra_lens': row['ra_lens_x'],
+                    'Dec_lens': row['dec_lens_x'],
+                    'cat_id': row['unique_id_x']}
+
+        if self.rng is not None:
+            offsets = self._random_offsets(row, self.rng)
+        else:
+            offsets = None
+        srcsP_bulge = self._extract_source_params(row, 'bulge', offsets=offsets)
+        srcsP_disk = self._extract_source_params(row, 'disk', offsets=offsets)
+
+        return lens_cat, srcsP_bulge, srcsP_disk
+
+    def __len__(self):
+        return len(self.df)
+
+    @staticmethod
+    def _random_offsets(row, rng):
+        # The original code used the disk component for computing
+        # the offsets for both the bulge and disk components.
+        Reff = np.sqrt(row['minor_axis_disk']*row['major_axis_disk'])
+        qs = row['minor_axis_disk']/row['major_axis_disk']
+        # The original code had this unexpected ordering of dys2 and dys1:
+        dys2, dys1 = random_location(Reff, qs, row['position_angle_x'],
+                                     row['sindex_disk'], rng)
+        # Return the offsets in the expected order:
+        return dys1, dys2
+
+    @staticmethod
+    def _extract_source_params(row, component, offsets=None):
+        Reff = np.sqrt(row[f'minor_axis_{component}']
+                       *row[f'major_axis_{component}'])
+        qs = row[f'minor_axis_{component}']/row[f'major_axis_{component}']
+        ys1 = row['x_src']
+        ys2 = row['y_src']
+        if offsets is not None:
+            ys1 -= offsets[0]
+            ys2 -= offsets[1]
+        return {'ys1': ys1,
+                'ys2': ys2,
+                'mag_src_u': row[f'magnorm_{component}_u'],
+                'mag_src_g': row[f'magnorm_{component}_g'],
+                'mag_src_r': row[f'magnorm_{component}_r'],
+                'mag_src_i': row[f'magnorm_{component}_i'],
+                'mag_src_z': row[f'magnorm_{component}_z'],
+                'mag_src_y': row[f'magnorm_{component}_y'],
+                'Reff_src': Reff,
+                'qs': qs,
+                'phs': row['position_angle_x'],
+                'ns': row[f'sindex_{component}'],
+                'zs': row['redshift_x'],
+                'sed_src': row[f'sed_{component}_host'],
+                'components': component}
